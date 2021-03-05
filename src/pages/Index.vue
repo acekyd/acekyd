7e72bcfbe0f20d18a666fa84c13d8b05f184273a
Adewale Abati <adewale@flutterwavego.com>
2021-03-05 15:40:10 +0100

    Update website landing page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,36 +3,9 @@
     <!-- Author intro -->
     <Header :show-title="true" />
 
-    <!-- List posts -->
-    <div class="posts">
-      <PostCard v-for="edge in $page.posts.edges" :key="edge.node.id" :post="edge.node"/>
-    </div>
-
   </Layout>
 </template>
 
-<page-query>
-query {
-  posts: allPost(filter: { published: { eq: true }}) {
-    edges {
-      node {
-        id
-        title
-        date (format: "D. MMMM YYYY")
-        timeToRead
-        description
-        cover_image (width: 770, height: 380, blur: 10)
-        path
-        tags {
-          id
-          title
-          path
-        }
-      }
-    }
-  }
-}
-</page-query>
 
 <script>
 import Header from '~/components/Header.vue'
@@ -49,7 +22,7 @@ export default {
       {
         key: 'og:title',
         property: 'og:title',
-        content: 'Adewale Abati - Blog!',
+        content: 'Adewale Abati - Web Engineer, Developer Advocate and Human!',
       }
     ]
   }
