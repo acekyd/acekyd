06121d474850dc413fcbcc702c94bf9216ec968d
Adewale Abati <acekyd01@gmail.com>
2022-09-03 19:27:55 +0000

    Update 6 files

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -124,11 +124,11 @@ query Post ($id: ID!) {
       margin-top: 0;
     }
 
-    p:first-of-type {
+    /* p:first-of-type {
       font-size: 1.2em;
       color: var(--title-color);
     }
-
+    */
     img {
       width: calc(100% + var(--space) * 2);
       margin-left: calc(var(--space) * -1);
